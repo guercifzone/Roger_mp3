d21e5d80293eb97f7dab3691308df3f4e69f54e6
صديق عبد الفتاح <  >
2017-03-31 14:00:06 +0100

    the first code of  mp3 player

---
+++ Roger v.3.1/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.TrackNameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SelectTrackToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PlayPauseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PreviousToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RepeatNoneToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RepeatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RepeatALLToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NoneToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RandomOffToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListBox_Track = New System.Windows.Forms.ListBox()
+        Me.Btn_hide = New System.Windows.Forms.Button()
+        Me.Btn_load = New System.Windows.Forms.Button()
+        Me.WMP = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Txt_Load = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.ContextMenuStrip.SuspendLayout()
+        CType(Me.WMP, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.ContextMenuStrip = Me.ContextMenuStrip
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "NotifyIcon"
+        Me.NotifyIcon.Visible = True
+        '
+        'ContextMenuStrip
+        '
+        Me.ContextMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TrackNameToolStripMenuItem, Me.ToolStripSeparator1, Me.SelectTrackToolStripMenuItem, Me.PlayPauseToolStripMenuItem, Me.NextToolStripMenuItem, Me.PreviousToolStripMenuItem, Me.RepeatNoneToolStripMenuItem, Me.RandomOffToolStripMenuItem, Me.ToolStripSeparator2, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip.Name = "ContextMenuStrip"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(151, 192)
+        '
+        'TrackNameToolStripMenuItem
+        '
+        Me.TrackNameToolStripMenuItem.Name = "TrackNameToolStripMenuItem"
+        Me.TrackNameToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.TrackNameToolStripMenuItem.Text = "&Track Name"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(147, 6)
+        '
+        'SelectTrackToolStripMenuItem
+        '
+        Me.SelectTrackToolStripMenuItem.Name = "SelectTrackToolStripMenuItem"
+        Me.SelectTrackToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.SelectTrackToolStripMenuItem.Text = "&Select Track"
+        '
+        'PlayPauseToolStripMenuItem
+        '
+        Me.PlayPauseToolStripMenuItem.Name = "PlayPauseToolStripMenuItem"
+        Me.PlayPauseToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.PlayPauseToolStripMenuItem.Text = "&Play/Pause"
+        '
+        'NextToolStripMenuItem
+        '
+        Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.NextToolStripMenuItem.Text = "&Next"
+        '
+        'PreviousToolStripMenuItem
+        '
+        Me.PreviousToolStripMenuItem.Name = "PreviousToolStripMenuItem"
+        Me.PreviousToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.PreviousToolStripMenuItem.Text = "&Previous"
+        '
+        'RepeatNoneToolStripMenuItem
+        '
+        Me.RepeatNoneToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RepeatToolStripMenuItem, Me.RepeatALLToolStripMenuItem, Me.NoneToolStripMenuItem})
+        Me.RepeatNoneToolStripMenuItem.Name = "RepeatNoneToolStripMenuItem"
+        Me.RepeatNoneToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.RepeatNoneToolStripMenuItem.Text = "&Repeat [None]"
+        '
+        'RepeatToolStripMenuItem
+        '
+        Me.RepeatToolStripMenuItem.Name = "RepeatToolStripMenuItem"
+        Me.RepeatToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
+        Me.RepeatToolStripMenuItem.Text = "&Repeat Track"
+        '
+        'RepeatALLToolStripMenuItem
+        '
+        Me.RepeatALLToolStripMenuItem.Name = "RepeatALLToolStripMenuItem"
+        Me.RepeatALLToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
+        Me.RepeatALLToolStripMenuItem.Text = "&Repeat ALL"
+        '
+        'NoneToolStripMenuItem
+        '
+        Me.NoneToolStripMenuItem.Name = "NoneToolStripMenuItem"
+        Me.NoneToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
+        Me.NoneToolStripMenuItem.Text = "&None"
+        '
+        'RandomOffToolStripMenuItem
+        '
+        Me.RandomOffToolStripMenuItem.Name = "RandomOffToolStripMenuItem"
+        Me.RandomOffToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.RandomOffToolStripMenuItem.Text = "&Random [Off]"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(147, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(150, 22)
+        Me.ExitToolStripMenuItem.Text = "&Exit"
+        '
+        'ListBox_Track
+        '
+        Me.ListBox_Track.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ListBox_Track.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ListBox_Track.FormattingEnabled = True
+        Me.ListBox_Track.Location = New System.Drawing.Point(13, 13)
+        Me.ListBox_Track.Name = "ListBox_Track"
+        Me.ListBox_Track.Size = New System.Drawing.Size(309, 186)
+        Me.ListBox_Track.TabIndex = 1
+        '
+        'Btn_hide
+        '
+        Me.Btn_hide.BackColor = System.Drawing.Color.White
+        Me.Btn_hide.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Btn_hide.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn_hide.Location = New System.Drawing.Point(13, 285)
+        Me.Btn_hide.Name = "Btn_hide"
+        Me.Btn_hide.Size = New System.Drawing.Size(309, 23)
+        Me.Btn_hide.TabIndex = 2
+        Me.Btn_hide.Text = "&Hide Me"
+        Me.Btn_hide.UseVisualStyleBackColor = False
+        '
+        'Btn_load
+        '
+        Me.Btn_load.BackColor = System.Drawing.Color.White
+        Me.Btn_load.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Btn_load.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn_load.Location = New System.Drawing.Point(237, 256)
+        Me.Btn_load.Name = "Btn_load"
+        Me.Btn_load.Size = New System.Drawing.Size(85, 23)
+        Me.Btn_load.TabIndex = 3
+        Me.Btn_load.Text = "&Load Folder"
+        Me.Btn_load.UseVisualStyleBackColor = False
+        '
+        'WMP
+        '
+        Me.WMP.Enabled = True
+        Me.WMP.Location = New System.Drawing.Point(13, 205)
+        Me.WMP.Name = "WMP"
+        Me.WMP.OcxState = CType(resources.GetObject("WMP.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.WMP.Size = New System.Drawing.Size(309, 45)
+        Me.WMP.TabIndex = 4
+        '
+        'Txt_Load
+        '
+        Me.Txt_Load.Location = New System.Drawing.Point(13, 258)
+        Me.Txt_Load.Name = "Txt_Load"
+        Me.Txt_Load.ReadOnly = True
+        Me.Txt_Load.Size = New System.Drawing.Size(218, 20)
+        Me.Txt_Load.TabIndex = 5
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Black
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 311)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(124, 13)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "&My Youtube Channel"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Honeydew
+        Me.ClientSize = New System.Drawing.Size(334, 332)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Txt_Load)
+        Me.Controls.Add(Me.WMP)
+        Me.Controls.Add(Me.Btn_load)
+        Me.Controls.Add(Me.Btn_hide)
+        Me.Controls.Add(Me.ListBox_Track)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Roger v.3.1"
+        Me.ContextMenuStrip.ResumeLayout(False)
+        CType(Me.WMP, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents NotifyIcon As System.Windows.Forms.NotifyIcon
+    Friend WithEvents ContextMenuStrip As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents TrackNameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SelectTrackToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PlayPauseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PreviousToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RepeatNoneToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RandomOffToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RepeatToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RepeatALLToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NoneToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ListBox_Track As System.Windows.Forms.ListBox
+    Friend WithEvents Btn_hide As System.Windows.Forms.Button
+    Friend WithEvents Btn_load As System.Windows.Forms.Button
+    Friend WithEvents WMP As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Txt_Load As System.Windows.Forms.TextBox
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ Roger v.3.1/Form1.vb
@@ -0,0 +1,201 @@
+﻿Public Class Form1
+    Dim currentTrack As Integer
+    Dim repeatMode As String = "none"
+    Dim gen As System.Random = New System.Random()
+    Dim random As Boolean = False
+    Dim tempInt As Integer
+
+#Region " movin form"
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        drag = True
+        mousex = Windows.Forms.Cursor.Position.X - Me.Left
+        mousey = Windows.Forms.Cursor.Position.Y - Me.Top
+    End Sub
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        If drag Then
+            Me.Top = Windows.Forms.Cursor.Position.Y - mousey
+            Me.Left = Windows.Forms.Cursor.Position.X - mousex
+        End If
+    End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp
+        drag = False
+    End Sub
+#End Region
+
+
+    Private Sub Txt_Load_TextChanged(sender As Object, e As EventArgs) Handles Txt_Load.TextChanged
+        ListBox_Track.Items.Clear()
+        If Not Txt_Load.Text = "" Then
+            For Each file As String In My.Computer.FileSystem.GetFiles(Txt_Load.Text, Microsoft.VisualBasic.FileIO.SearchOption.SearchTopLevelOnly, "*.mp3")
+                ListBox_Track.Items.Add(file)
+            Next
+        End If
+    End Sub
+    Private Sub ListBox_Track_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox_Track.SelectedIndexChanged
+        WMP.URL = ListBox_Track.SelectedItem.ToString
+        currentTrack = ListBox_Track.SelectedIndex
+    End Sub
+    Private Sub NotifyIcon_DoubleClick(sender As Object, e As EventArgs) Handles NotifyIcon.DoubleClick
+        Me.Visible = True
+    End Sub
+    Private Sub WMP_MediaChange(sender As Object, e As AxWMPLib._WMPOCXEvents_MediaChangeEvent) Handles WMP.MediaChange
+        TrackNameToolStripMenuItem.Text = ListBox_Track.Items(currentTrack)
+    End Sub
+    Private Sub WMP_PlayStateChange(sender As Object, e As AxWMPLib._WMPOCXEvents_PlayStateChangeEvent) Handles WMP.PlayStateChange
+        Static Dim play As Boolean = True
+
+        Select Case WMP.playState
+            Case 10
+                If play Then
+                    WMP.Ctlcontrols.play()
+
+                End If
+            Case 8
+                If random Then
+                    play = False
+                    tempInt = gen.Next(0, ListBox_Track.Items.Count + 1)
+                    WMP.URL = ListBox_Track.Items(tempInt)
+                    currentTrack = tempInt
+                    play = True
+                Else
+                    play = False
+                    If repeatMode = "track" Then
+                        WMP.URL = ListBox_Track.Items(currentTrack)
+                        play = True
+                        Exit Sub
+                    End If
+                    If currentTrack = (ListBox_Track.Items.Count - 1) Then
+                        If repeatMode = "all" Then
+                            WMP.URL = ListBox_Track.Items(0)
+                            currentTrack = 0
+                        End If
+                    Else
+                        WMP.URL = ListBox_Track.Items(currentTrack + 1)
+                        currentTrack += 1
+
+                    End If
+                End If
+                play = True
+
+        End Select
+    End Sub
+
+#Region "buton option"
+
+    Private Sub Btn_hide_MouseHover(sender As Object, e As EventArgs) Handles Btn_hide.MouseHover
+        '309; 23
+        Btn_hide.Size = New System.Drawing.Point(315, 25)
+        Btn_hide.ForeColor = Color.CadetBlue
+        Btn_hide.BackColor = Color.Black
+    End Sub
+
+    Private Sub Btn_hide_MouseLeave(sender As Object, e As EventArgs) Handles Btn_hide.MouseLeave
+        Btn_hide.Size = New System.Drawing.Point(309, 23)
+        Btn_hide.ForeColor = Color.Black
+        Btn_hide.BackColor = Color.White
+    End Sub
+    Private Sub Btn_hide_Click(sender As Object, e As EventArgs) Handles Btn_hide.Click
+        Me.Hide()
+    End Sub
+    Private Sub Btn_load_Click(sender As Object, e As EventArgs) Handles Btn_load.Click
+        Dim open As New FolderBrowserDialog
+        If open.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Txt_Load.Text = open.SelectedPath.ToString
+        End If
+    End Sub
+    Private Sub Btn_load_MouseHover(sender As Object, e As EventArgs) Handles Btn_load.MouseHover
+        '85; 23
+        Btn_load.Size = New System.Drawing.Point(90, 25)
+        Btn_load.ForeColor = Color.CadetBlue
+        Btn_load.BackColor = Color.Black
+    End Sub
+
+    Private Sub Btn_load_MouseLeave(sender As Object, e As EventArgs) Handles Btn_load.MouseLeave
+        Btn_load.Size = New System.Drawing.Point(85, 23)
+        Btn_load.ForeColor = Color.Black
+        Btn_load.BackColor = Color.White
+    End Sub
+#End Region
+
+#Region "tools menuscript"
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub SelectTrackToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectTrackToolStripMenuItem.Click
+        Me.Visible = False
+    End Sub
+
+    Private Sub NextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NextToolStripMenuItem.Click
+        If currentTrack = (ListBox_Track.Items.Count - 1) Then
+            WMP.URL = ListBox_Track.Items(0)
+            currentTrack = 0
+        Else
+
+            WMP.URL = ListBox_Track.Items(currentTrack + 1)
+            currentTrack += 1
+
+        End If
+    End Sub
+
+    Private Sub PreviousToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PreviousToolStripMenuItem.Click
+        If currentTrack = 0 Then
+            WMP.URL = ListBox_Track.Items(ListBox_Track.Items.Count - 1)
+            currentTrack = ListBox_Track.Items.Count - 1
+        Else
+
+            WMP.URL = ListBox_Track.Items(currentTrack - 1)
+            currentTrack -= 1
+
+        End If
+    End Sub
+
+    Private Sub PlayPauseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PlayPauseToolStripMenuItem.Click
+        If WMP.playState = WMPLib.WMPPlayState.wmppsPlaying Then
+            WMP.Ctlcontrols.pause()
+        ElseIf WMP.playState = WMPLib.WMPPlayState.wmppsPaused Then
+            WMP.Ctlcontrols.play()
+
+        End If
+    End Sub
+
+    Private Sub RepeatToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RepeatToolStripMenuItem.Click
+        repeatMode = "track"
+        RepeatNoneToolStripMenuItem.Text = "Repeat [Track]"
+    End Sub
+
+    Private Sub RepeatALLToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RepeatALLToolStripMenuItem.Click
+        repeatMode = "ALL"
+        RepeatNoneToolStripMenuItem.Text = "Repeat [ALL]"
+    End Sub
+
+    Private Sub NoneToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NoneToolStripMenuItem.Click
+        repeatMode = "None"
+        RepeatNoneToolStripMenuItem.Text = "Repeat [None]"
+    End Sub
+
+    Private Sub RandomOffToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RandomOffToolStripMenuItem.Click
+        If random = True Then
+            random = False
+            RandomOffToolStripMenuItem.Text = "Random {Off}"
+        Else
+            random = True
+            RandomOffToolStripMenuItem.Text = "Random {On}"
+        End If
+    End Sub
+
+#End Region
+
+
+
+
+
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim url As String = "https://www.youtube.com/channel/UCZr2VGmXdJ-yGLUZztCVFGw"
+        Process.Start(url)
+    End Sub
+End Class
